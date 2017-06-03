0d9faa4689692b7420f3fd003ea24a549dfdb756
tashf33n <tashfeen65@gmail.com>
2017-06-03 15:25:42 +0500

    Call Web Api

---
+++ ScrapeClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using ScrapeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape myScarpe = new Scrape();
+
+            string value = myScarpe.ScrapeWebpage("https://graph.facebook.com/youtube?access_token=EAACW5Fg5N2IBADv8LvWFIqMyj51WODAKdQhnH7B83ZBZB9RwxBrN79TJZAcI2YuBYzE9psjn7btYceQFCu5nTmAkRNqFXDPi63LzjziYx86DEoZBVNnFIhGdw1rWc0YrEW2Hqj4ajdT9uOAifTIkiM4GAPWSZA7OJJqTsLbXYduZCCupH6Fot6");
+
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
